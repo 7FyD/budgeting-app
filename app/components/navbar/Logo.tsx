b633c82c440846884587af2d72aed17c8a41a139
7FyD <80410162+7FyD@users.noreply.github.com>
2023-08-30 03:42:34 +0300

    Very basic navbar implementation

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      onClick={() => router.push("/")}
+      className="cursor-pointer"
+      src="/images/logo.png"
+      height="75"
+      width="75"
+      alt="Logo"
+    />
+  );
+};
+
+export default Logo;
